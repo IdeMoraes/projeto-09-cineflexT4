c45b651cae97d083cb5d535445696294d751bfe7
Igor de Moraes <r.de.moraes.igor@gmail.com>
2021-12-20 20:32:20 -0300

    Prazo final 20h30

--- src/components/Assento/index.js
+++ src/components/Assento/index.js
@@ -1,12 +1,22 @@
 import './style.css';
 
 export default function Assento({id,name,isAvailable}){
+
+  //const[ids,setIds]=useState([]);
+
   return(
-    isAvailable===true ? (<div className="assento disponivel" >{name}</div>):(<div className="assento indisponivel" onClick={indisponivel}>{name}</div>)
+    isAvailable===true ? (<div className="assento disponivel" id={id} onClick={()=>selecionar(id)}>{name}</div>):(<div className="assento indisponivel" onClick={indisponivel}>{name}</div>)
     
   );
 }
 
 function indisponivel(){
   alert("Esse assento não está disponível");
+}
+
+let selecionados =[]
+
+function selecionar(banana){
+  selecionados.push(banana)
+  console.log(selecionados);
 }
--- src/components/TelaSessao/index.js
+++ src/components/TelaSessao/index.js
@@ -48,4 +48,6 @@ export default function TelaSessao(props) {
         </div>
         </>
     );
+
 }
+
